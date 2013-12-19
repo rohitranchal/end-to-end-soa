bdf5207922c3978b03f93b3fb18bc52f099ca0f4
Ruchith Fernando <ruchith@Ruchiths-MacBook-Pro.local>
2013-12-19 00:08:54 -0500

    Initializing the database with default values

--- node/db.sql
+++ node/db.sql
@@ -1,10 +1,14 @@
 DROP TABLE Service;
 CREATE TABLE Service (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	trust_level INT,
-	name VARCHAR(2048)
+	name VARCHAR(2048),
+	trust_level FLOAT
 );
 
+INSERT INTO Service(name, trust_level) VALUES
+('localhost:3001', 10.0),
+('localhost:3002', 5.0),
+('localhost:3003', 4.0);
 
 DROP TABLE Interaction;
 CREATE TABLE Interaction (
