2bfc261ab74c5bd3beb9f5578ec96b9b0b60821b
Ruchith Fernando <ruchith@Ruchiths-MacBook-Pro.local>
2013-12-27 12:02:02 -0500

    Bug fix: check whether callback function is available

--- node/monitor/trust_algo/index.js
+++ node/monitor/trust_algo/index.js
@@ -8,7 +8,9 @@ var simple_trust = function simple_trust_update(from, to, cb) {
 	if(from.trust_level != 0) {
 		var new_trust_level = from.trust_level * to.trust_level/from.trust_level;
 		db.set_service_trust_level(from.id, new_trust_level);
-		cb(new_trust_level);	
+		if(typeof cb != 'undefined') {
+			cb(new_trust_level);
+		}
 	}
 };
 
