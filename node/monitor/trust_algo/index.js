5444a5ab892b6bd7251ccb5552919e19add6664c
Rohit Ranchal <rranchal@purdue.edu>
2014-01-20 16:41:53 -0500

    Deployed adblock policy and request in service monitor and updated default active trust algo to new policy in .../trust_algo/index.js

--- node/monitor/trust_algo/index.js
+++ node/monitor/trust_algo/index.js
@@ -8,11 +8,11 @@ console.log('Loading trust algorithms ... ');
 var files = fs.readdirSync('./trust_algo/passive/');
 for(var i = 0; i < files.length; i++) {
 	console.log('Reading : ' + files[i]);
-	passive_algos[passive_algos.length] = require('./passive/' + files[i]);	
+	passive_algos[passive_algos.length] = require('./passive/' + files[i]);
 }
 var files = fs.readdirSync('./trust_algo/active/');
 for(var i = 0; i < files.length; i++) {
-	
+
 	if(files[i].indexOf('.js', files[i].length - 3) !== -1) {
 		console.log('Reading : ' + files[i]);
 		active_algos[active_algos.length] = require('./active/' + files[i]);
@@ -20,19 +20,19 @@ for(var i = 0; i < files.length; i++) {
 }
 
 var default_trust_algo = 4;
-var default_active_trust_algo = 2;
+var default_active_trust_algo = 3;
 
 exports.set_default_algo = function(algo_id_str) {
 	algo_id = algo_id_str.substring(1);
 	if(algo_id_str.indexOf('p') == 0) {
 		//Passive algo
 		if(algo_id >= 0 || algo_id < passive_algos.length) {
-			default_trust_algo = algo_id;	
+			default_trust_algo = algo_id;
 		}
 	} else if(algo_id_str.indexOf('a') == 0) {
 		//Active algo
 		if(algo_id >= 0 || algo_id < active_algos.length) {
-			default_active_trust_algo = algo_id;	
+			default_active_trust_algo = algo_id;
 		}
 	}
 };
@@ -64,14 +64,14 @@ exports.algo_list = function(cb) {
 	var a_a = new Array();
 
 	for(var i = 0; i < passive_algos.length; i++) {
-		p_a[i] = {id: i, 
-					name : passive_algos[i].name, 
+		p_a[i] = {id: i,
+					name : passive_algos[i].name,
 					alg : String(passive_algos[i].alg)};
 	}
 
 	for(var i = 0; i < active_algos.length; i++) {
-		a_a[i] = {id: i, 
-					name : active_algos[i].name, 
+		a_a[i] = {id: i,
+					name : active_algos[i].name,
 					alg : String(active_algos[i].alg),
 					auth : String(active_algos[i].authorize)};
 	}
