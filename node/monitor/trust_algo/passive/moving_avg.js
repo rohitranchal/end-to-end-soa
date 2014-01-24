ccc4387321c7230239421351f69f3313b046e810
Rohit Ranchal <rranchal@purdue.edu>
2014-01-23 21:01:15 -0500

    Added simple average and moving average trust algos for the passive case

--- node/monitor/trust_algo/passive/moving_avg.js
+++ node/monitor/trust_algo/passive/moving_avg.js
@@ -1,7 +1,17 @@
 var db = require('../../db');
 
+var moving_avg_weight = 0.8;
+
 var moving_avg_trust = function moving_avg_trust_update(from, to) {
-	//TODO
+	if(from.trust_level != 0) {
+    var new_trust_level = from.trust_level * moving_avg_weight + to.trust_level * (1 - moving_avg_weight);
+    db.set_service_trust_level(from.id, new_trust_level);
+    if(typeof cb != 'undefined') {
+      cb(new_trust_level);
+    }
+  }
 };
 
 module.exports = {name : 'Moving Average', alg : moving_avg_trust};
+
+
