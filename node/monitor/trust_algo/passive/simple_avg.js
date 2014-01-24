e9b6b2d040d7623f4c4f592ea1692cf3e8a663a0
Rohit Ranchal <rranchal@purdue.edu>
2014-01-24 14:05:26 -0500

    Fixed some code without change in functionality in moving and simple trust algos

--- node/monitor/trust_algo/passive/simple_avg.js
+++ node/monitor/trust_algo/passive/simple_avg.js
@@ -2,7 +2,8 @@ var db = require('../../db');
 
 var simple_avg_trust = function simple_avg_trust_update(from, to) {
   if(from.trust_level != 0) {
-    var new_trust_level = ((from.trust_level + to.trust_level)/2).toFixed(3);
+    var tmp_trust_level = ((from.trust_level + to.trust_level)/2);
+    var new_trust_level = tmp_trust_level.toFixed(3);
     db.set_service_trust_level(from.id, new_trust_level);
     if(typeof cb != 'undefined') {
       cb(new_trust_level);
