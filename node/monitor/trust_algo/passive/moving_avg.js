d6e002df64d0d965a64faaae810379df90b0879c
Rohit Ranchal <rranchal@purdue.edu>
2014-01-24 13:54:02 -0500

    Limited decimal places for trust values to 3 in trust algos

--- node/monitor/trust_algo/passive/moving_avg.js
+++ node/monitor/trust_algo/passive/moving_avg.js
@@ -4,7 +4,7 @@ var moving_avg_weight = 0.8;
 
 var moving_avg_trust = function moving_avg_trust_update(from, to, cb) {
 	if(from.trust_level != 0) {
-		var new_trust_level = from.trust_level * moving_avg_weight + to.trust_level * (1 - moving_avg_weight);
+		var new_trust_level = (from.trust_level * moving_avg_weight + to.trust_level * (1 - moving_avg_weight)).toFixed(3);
 		db.set_service_trust_level(from.id, new_trust_level);
 		if(typeof cb != 'undefined') {
 			console.log('Calling cb');
