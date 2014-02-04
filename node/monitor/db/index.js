a9566aa1e52ef8f69022c206e98eb0ea867f09db
mazarmi <m.azarmi@gmail.com>
2014-02-04 13:03:45 -0500

    Changed my password to empty (the same as original one).

--- node/monitor/db/index.js
+++ node/monitor/db/index.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : 'ngc123',
+	password : '',
 	database : 'soa_trust',
 	multipleStatements: true
 });
