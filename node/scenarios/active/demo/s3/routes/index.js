83f4dfe05f26f8a90bce4a43b490fcb2bff7d40b
Ruchith Fernando <ruchith.fernando@gmail.com>
2014-05-11 19:18:04 -0400

    Payment Gateway service now exposes an HTTPS endpoint. S2, S3, S4 updated to use https endpoint.

--- node/scenarios/active/demo/s3/routes/index.js
+++ node/scenarios/active/demo/s3/routes/index.js
@@ -1,9 +1,11 @@
 var request = require('../../../../../instr_request_block');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 exports.index = function(req, res) {
 
 		global.my_url = 'http://' + global.my_host + ':' + global.my_port;
-		request('http://' + global.my_host + ':4113/', function (error, response, body) {
+		request('https://' + global.my_host + ':4113/', function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var val = JSON.parse(body);
 				val.price += 200;
--- node/scenarios/active/demo/s5/app.js
+++ node/scenarios/active/demo/s5/app.js
@@ -1,11 +1,9 @@
-
-/**
- * Module dependencies.
- */
+var fs = require('fs');
 
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
+var https = require('https');
 var path = require('path');
 
 var app = express();
@@ -44,6 +42,18 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/dos_attack', routes.dos_attack);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port') + 1000, function(){
+  console.log('Express server listening on port ' + (app.get('port') + 1000));
+});
+
+//To be able to start from the monitor 
+var key_path_prefix = '../scenarios/active/demo/s5/';
+
+var private_key = fs.readFileSync(key_path_prefix + './keys/privatekey.pem').toString();
+var certificate = fs.readFileSync(key_path_prefix + './keys/certificate.pem').toString();
+
+var credentials = {key: private_key, cert: certificate};
+
+https.createServer(credentials, app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
